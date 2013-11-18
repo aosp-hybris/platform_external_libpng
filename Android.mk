1a8db00c9536510fc71496322534c53cea37baf3
Jani Monoses <jani@ubuntu.com>
2013-11-18 15:05:03 +0200

    disable build with clang
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- Android.mk
+++ Android.mk
@@ -59,7 +59,7 @@ include $(BUILD_HOST_STATIC_LIBRARY)
 # =====================================================
 
 include $(CLEAR_VARS)
-LOCAL_CLANG := true
+#LOCAL_CLANG := true
 LOCAL_SRC_FILES := $(common_SRC_FILES)
 LOCAL_CFLAGS += $(common_CFLAGS) -ftrapv
 LOCAL_C_INCLUDES += $(common_C_INCLUDES) \
@@ -75,7 +75,7 @@ include $(BUILD_STATIC_LIBRARY)
 # =====================================================
 
 include $(CLEAR_VARS)
-LOCAL_CLANG := true
+#LOCAL_CLANG := true
 LOCAL_SRC_FILES := $(common_SRC_FILES)
 LOCAL_CFLAGS += $(common_CFLAGS) -ftrapv
 LOCAL_C_INCLUDES += $(common_C_INCLUDES) \
@@ -94,7 +94,7 @@ include $(BUILD_SHARED_LIBRARY)
 # =====================================================
 
 include $(CLEAR_VARS)
-LOCAL_CLANG := true
+#LOCAL_CLANG := true
 LOCAL_C_INCLUDES:= $(common_C_INCLUDES) external/zlib
 LOCAL_SRC_FILES:= pngtest.c
 LOCAL_MODULE := pngtest
