e130a2e3ef563b679b0c854ccd8ec6272eadbe1c
Yen-Chin Lee <coldnew.tw@gmail.com>
2014-08-29 21:39:46 +0800

    Disable build with clang
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- Android.mk
+++ Android.mk
@@ -29,11 +29,11 @@ ifeq ($(HOST_OS),windows)
     # Case where we're building windows but not under linux (so it must be cygwin)
     # In this case, gcc cygwin doesn't recognize -fvisibility=hidden
     $(info libpng: Ignoring gcc flag $(common_CFLAGS) on Cygwin)
-    common_CFLAGS := 
+    common_CFLAGS :=
   endif
 endif
 
-common_C_INCLUDES += 
+common_C_INCLUDES +=
 
 common_COPY_HEADERS_TO := libpng
 common_COPY_HEADERS := png.h pngconf.h pngusr.h
@@ -59,7 +59,7 @@ include $(BUILD_HOST_STATIC_LIBRARY)
 # =====================================================
 
 include $(CLEAR_VARS)
-LOCAL_CLANG := true
+#LOCAL_CLANG := true
 LOCAL_SRC_FILES := $(common_SRC_FILES)
 LOCAL_CFLAGS += $(common_CFLAGS) -ftrapv
 LOCAL_C_INCLUDES += $(common_C_INCLUDES) \
